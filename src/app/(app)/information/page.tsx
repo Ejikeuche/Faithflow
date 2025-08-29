50add750ae7becad9c4c4ed7eac3019611d5d913
Ejike U <ejike.uche.EU@gmail.com>
2025-08-29 17:52:56 +0000

    Runtime Error
    Error: AlertDialogTrigger is not defined
    
    src/app/(app)/

--- src/app/(app)/information/page.tsx
+++ src/app/(app)/information/page.tsx
@@ -56,6 +56,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 
 const initialInformation: Information[] = [
