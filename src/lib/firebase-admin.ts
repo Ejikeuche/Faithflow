28931b063b748f3c1de0e5aa530386e7739b2c87
Ejike U <ejike.uche.EU@gmail.com>
2025-09-01 14:55:27 +0000

    The signup page is not opening when I click to tab

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,17 +1,15 @@
 
 import admin from 'firebase-admin';
 
-// Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      credential: admin.credential.applicationDefault(),
     });
   } catch (error: any) {
     console.error('Firebase admin initialization error', error.stack);
   }
 }
 
-// Export the initialized services
 export const adminAuth = admin.auth();
 export const adminDb = admin.firestore();
