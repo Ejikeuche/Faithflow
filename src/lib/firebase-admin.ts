c7b894d81d2a880f41445cd44ce06b7a823d399f
Ejike U <ejike.uche.EU@gmail.com>
2025-09-04 06:35:27 +0000

    Error: 2 UNKNOWN: Getting metadata from plugin failed with error: Could

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,10 +3,20 @@ import admin from 'firebase-admin';
 // Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
   try {
-    // In a Firebase/Google Cloud environment, initializeApp() with no arguments
-    // will automatically use the service account associated with the environment.
-    admin.initializeApp();
-    console.log('Firebase Admin SDK Initialized.');
+    const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS ? 
+      JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS) : undefined;
+
+    if (serviceAccount) {
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+      });
+      console.log('Firebase Admin SDK Initialized with service account.');
+    } else {
+       // In a Firebase/Google Cloud environment, initializeApp() with no arguments
+      // will automatically use the service account associated with the environment.
+      admin.initializeApp();
+      console.log('Firebase Admin SDK Initialized using default credentials.');
+    }
   } catch (error: any) {
     console.error('Firebase admin initialization error:', error.stack);
   }
